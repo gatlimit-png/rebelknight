9e6703553f2239392b025b01918bd742421ed11a
gatlimit-png <gatlimit@gmail.com>
2026-09-18 21:34:43 +0300

    Kotlin metadata versiyon kontrol atlama argumani eklendi

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.8.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -62,6 +62,7 @@ subprojects {
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
                     "-Xno-receiver-assertions"
+                    "-Xskip-metadata-version-check"
                 )
             }
         }
